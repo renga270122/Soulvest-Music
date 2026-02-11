b5151db4b19198ee21a55b8aa1a45da7d10276c9
Renganathan M <srirenganathan.87@gmail.com>
2026-02-11 11:22:12 +0530

    Remove CNAME entry

---
+++ soulvest_music/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Personal\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Personal\\Soulvest_Music\\soulvest_music" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Personal\\flutter"
+  "PROJECT_DIR=D:\\Personal\\Soulvest_Music\\soulvest_music"
+  "FLUTTER_ROOT=D:\\Personal\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Personal\\Soulvest_Music\\soulvest_music\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Personal\\Soulvest_Music\\soulvest_music"
+  "FLUTTER_TARGET=D:\\Personal\\Soulvest_Music\\soulvest_music\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OWY0NTVkMjQ4Ng==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDI5MTM2MzJhNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Personal\\Soulvest_Music\\soulvest_music\\.dart_tool\\package_config.json"
+)
